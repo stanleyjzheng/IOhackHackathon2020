bfe10830bba17380f00aa0d661fc4bbc9c2c26ba
Rougg <35879277+Rougg@users.noreply.github.com>
2020-06-01 00:12:56 -0600

    Final Version
    Sorry for the delay (we experienced wifi issues which prevented us from submitting and are unfortunately still impeding us a little bit), but this is the final github version. Thanks!

--- next_in_queue.py
+++ next_in_queue.py
@@ -6,7 +6,7 @@ def next_in_queue(store):
     Returns the person in queue priority 1 for a given store.
     """
     first_line = open(f"{store}_queue.csv", "r").readline().strip().split(",")
-    return first_line[1]
+    return first_line[0]
 
 
 print(next_in_queue(sys.argv[1]), end="")
