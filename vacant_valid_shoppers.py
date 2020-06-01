b81961d0071f16ba83730d0020166c6be2f7f4f6
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2020-05-31 23:22:20 -0600

    Create vacant_valid_shoppers.py

---
+++ vacant_valid_shoppers.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def vacant_valid_shoppers(store):
+
+    # retrieve current number of valid shoppers
+    file = open(f"{store}_valid_shoppers.csv", "r")
+    num_valid_shoppers = len([line.strip() for line in file.readlines()])
+    file.close()
+
+    # retrieve current number of shoppers
+    file = open(f"{store}_current_shoppers.csv", "r")
+    num_shoppers = len([line.strip().split() for line in file.readlines()])
+    file.close()
+
+    # retrieve store capacity
+    file = open(f"companies.csv", "r")
+    companies = [line.strip().split() for line in file.readlines()]
+    for company in companies:
+        if company[0] == store:
+            capacity = int(company[3])
+            break
+
+    if capacity - num_shoppers - num_valid_shoppers >= 0:
+        return "True"
+    else:
+        return "False"
+
+
+print(capacity_left(sys.argv[1]), end="")
